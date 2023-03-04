4e29e08679174544ba63c0c5eef9a16ba7e0056f
Matt Veenstra <35970858+tribalmatt@users.noreply.github.com>
2023-03-04 12:52:29 -0800

    random ball and layer chgane

--- examples/piu/balls/main.js
+++ examples/piu/balls/main.js
@@ -14,9 +14,17 @@
 
 import {} from "piu/MC";
 
-const backgroundSkin = new Skin({ fill:"silver" });
+const backgroundSkin = new Skin({ fill:"black" });
+// const backgroundSkin = new Skin({ fill:"silver" });
+// const backgroundSkin = new Skin({ fill:"white" });
 const ballTexture = new Texture("balls.png");
 const ballSkin = new Skin({ texture:ballTexture, x:0, y:0, width:30, height:30, variants:30 });
+
+function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 class BallBehavior extends Behavior {
 	onCreate(ball, delta) {
@@ -31,27 +39,35 @@ class BallBehavior extends Behavior {
 		ball.start();
 	}
 	onTimeChanged(ball) {
-		var dx = this.dx;
-		var dy = this.dy;
-		ball.moveBy(dx, dy);
-		var x = this.x + dx;
-		var y = this.y + dy;
-		if ((x < 0) || (x > this.width)) dx = -dx;
-		if ((y < 0) || (y > this.height)) dy = -dy;
-		this.dx = dx;
-		this.dy = dy;
-		this.x = x;
-		this.y = y;
+		let x = this.x + this.dx;
+		let y = this.y + this.dy;
+		if ((x < 0) || (x > this.width)) {
+			// change delta direction and randomize value
+			this.dx = -(Math.sign(this.dx) * randomNumber(2, 6));
+			
+		}
+		if ((y < 0) || (y > this.height)) {
+			this.dy = -(Math.sign(this.dy) * randomNumber(2, 6));
+			if (y > this.height) {
+				// change the drawing order or z position
+				application.remove(ball);
+				application.add(ball);
+			}
+		}
+
+		this.x = this.x + this.dx;
+		this.y = this.y + this.dy;
+		ball.moveBy(this.dx, this.dy);
 	}
 };
 
 let BallApplication = Application.template($ => ({
 	skin:backgroundSkin,
 	contents: [
-		Content(6, { left:0, top:0, skin:ballSkin, variant:0, Behavior: BallBehavior } ),
-		Content(5, { right:0, top:0, skin:ballSkin, variant:1, Behavior: BallBehavior } ),
-		Content(4, { right:0, bottom:0, skin:ballSkin, variant:2, Behavior: BallBehavior } ),
-		Content(3, { left:0, bottom:0, skin:ballSkin, variant:3, Behavior: BallBehavior } ),
+		Content(6, { left:0, top:0, deltaX:6, deltaY:6, skin:ballSkin, variant:0, Behavior: BallBehavior } ),
+		Content(5, { right:0, top:0, deltaX:5, deltaY:5, skin:ballSkin, variant:1, Behavior: BallBehavior } ),
+		Content(4, { right:0, bottom:0, deltaX:4, deltaY:4, skin:ballSkin, variant:2, Behavior: BallBehavior } ),
+		Content(3, { left:0, bottom:0, deltaX:3, deltaY:3, skin:ballSkin, variant:3, Behavior: BallBehavior } ),
 	]
 }));
 
